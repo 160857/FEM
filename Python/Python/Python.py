d6f6297cfd5015784a91702eabc5319818dc207f
160857 <160857@stud.prz.edu.pl>
2021-04-08 13:32:42 +0200

    Słowniki

--- Python/Python/Python.py
+++ Python/Python/Python.py
@@ -27,17 +27,35 @@
 
 
 #Drożdże i Alkohol
-d=[10, 8, 10, 12, 6, 8, 7, 12, 10, 16, 16, 9, 14, 9, 11, 17, 18, 9, 5, 17, 11, 17, 7, 7, 12, 9, 5, 18, 6, 7, 9, 9, 6, 8, 8, 11, 13, 16, 8, 8, 12, 5, 18, 15, 17, 18, 7, 8, 13, 5, 12, 11, 11, 12, 5, 17, 7, 15, 10, 14, 18, 5, 8, 9, 10, 14, 15, 13, 16, 14, 17, 16, 10, 7, 14, 15, 17, 11, 10, 18, 18, 9, 12, 18, 12, 13, 7, 10, 16, 12, 16, 8, 11, 15, 8, 7, 7, 10, 13, 13]
+#d=[10, 8, 10, 12, 6, 8, 7, 12, 10, 16, 16, 9, 14, 9, 11, 17, 18, 9, 5, 17, 11, 17, 7, 7, 12, 9, 5, 18, 6, 7, 9, 9, 6, 8, 8, 11, 13, 16, 8, 8, 12, 5, 18, 15, 17, 18, 7, 8, 13, 5, 12, 11, 11, 12, 5, 17, 7, 15, 10, 14, 18, 5, 8, 9, 10, 14, 15, 13, 16, 14, 17, 16, 10, 7, 14, 15, 17, 11, 10, 18, 18, 9, 12, 18, 12, 13, 7, 10, 16, 12, 16, 8, 11, 15, 8, 7, 7, 10, 13, 13]
 #print (d[5])
 #print (d[7])
 #print (d[11])
 #print (d[17])
 #print (d[23])
 #print (d[50])
-
 #print (d[:15]) #wypisuje pierwsze 15 pozycji
-
 #d[12] = d[12] - 0.2 #Zmeiniamy wartosc
+#d.append(14)
+#print (d) Dodajemy nową pozycję do naszej Listy
+#extend łaczy listy
+#insert wstawianie w liste zmiennej
+#remove usuwa pierwsza wartosc z listy o tej wartosci
+#pop usuwa element o danym adresie
+#index szukanie wartosci z listy
+#reverse odwraca listę
 
-d.append(14)
-print (d)
+
+
+#dictionary = {'plytka1':101, 'plytka2':201, 'plytka3': 302, 'plytka4':102}
+#print (dictionary['plytka1'])
+#print (dictionary) #Wyswietla całość
+#print (dictionary.keys()) #Zwraca płytki
+#print (dictionary.values()) #Zwraca Wartosci
+
+#dictionary['plytka5']=0 # dodanie pustej płtyki
+#print (dictionary['plytka5'])
+#del dictionary
+#print (dictionary) Po usunieciu Słownika nie mozna go wypisac
+
+
