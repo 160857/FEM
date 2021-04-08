e965da2876c8a72895156000e1bd3b1def36eaff
160857 <160857@stud.prz.edu.pl>
2021-04-08 13:24:50 +0200

    Listy

--- Python/Python/Python.py
+++ Python/Python/Python.py
@@ -24,3 +24,20 @@
 #print (jakas_zmienna) 
 #jakas_zmienna = 8.5 
 #print (jakas_zmienna) Nie trzeba definować typu zmiennej 
+
+
+#Drożdże i Alkohol
+d=[10, 8, 10, 12, 6, 8, 7, 12, 10, 16, 16, 9, 14, 9, 11, 17, 18, 9, 5, 17, 11, 17, 7, 7, 12, 9, 5, 18, 6, 7, 9, 9, 6, 8, 8, 11, 13, 16, 8, 8, 12, 5, 18, 15, 17, 18, 7, 8, 13, 5, 12, 11, 11, 12, 5, 17, 7, 15, 10, 14, 18, 5, 8, 9, 10, 14, 15, 13, 16, 14, 17, 16, 10, 7, 14, 15, 17, 11, 10, 18, 18, 9, 12, 18, 12, 13, 7, 10, 16, 12, 16, 8, 11, 15, 8, 7, 7, 10, 13, 13]
+#print (d[5])
+#print (d[7])
+#print (d[11])
+#print (d[17])
+#print (d[23])
+#print (d[50])
+
+#print (d[:15]) #wypisuje pierwsze 15 pozycji
+
+#d[12] = d[12] - 0.2 #Zmeiniamy wartosc
+
+d.append(14)
+print (d)
