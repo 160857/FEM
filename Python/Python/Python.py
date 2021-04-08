a22391353a8811a064c7040828cb2d4b8a074f83
160857 <160857@stud.prz.edu.pl>
2021-04-08 13:12:05 +0200

    3 Podpunkty z objasnieniem

--- Python/Python/Python.py
+++ Python/Python/Python.py
@@ -1,4 +1,26 @@
 
-print ("Hello World")
+#print ("Hello World") Wypisuje na Hello World
 
 
+#tekst = "Lorem ipsum dolor sit amen, ..."
+#print (tekst)
+#Zwraca nam tesks
+
+
+
+
+#paliwo = 8.5 * (67*0.1 + 124*0.15 + 11.7*0.18) #67 zwykla droga, 124 autobahn, 11,7 grunt
+#autostrada = 124*0.17
+#prom = 50
+#koszt = paliwo + autostrada + prom
+#print ("Samemu", koszt)
+#print ("+1 osoba", koszt/2)
+#print ("+2 osoba", koszt/3)
+#print ("+3 osoba", koszt/4)
+#print ("+4 osoba", koszt/5)
+
+
+#jakas_zmienna = "To jest jakis napis" 
+#print (jakas_zmienna) 
+#jakas_zmienna = 8.5 
+#print (jakas_zmienna) Nie trzeba definować typu zmiennej 
